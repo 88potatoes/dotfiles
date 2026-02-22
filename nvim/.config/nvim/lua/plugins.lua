c091f33348af0573b703e200c13d84fedb3639a1
Eric <ericll360@gmail.com>
2026-02-22 12:54:56 +1100

    rm some more

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -107,18 +107,6 @@ require("lazy").setup({
       require('config.comment')
     end
   },
-  -- Git blame
-  {
-    "f-person/git-blame.nvim",
-    event = "VeryLazy",
-    opts = {
-      enabled = true,
-      message_template = " <summary> • <date> • <author> • <<sha>>",
-      date_format = "%m-%d-%Y %H:%M:%S",
-      virtual_text_column = 1,
-    }
-  },
-
   -- LazyGit
   {
     "kdheepak/lazygit.nvim",
@@ -308,5 +296,3 @@ require("lazy").setup({
     opts = {},
   }
 })
-
-require("gitsigns").setup()
