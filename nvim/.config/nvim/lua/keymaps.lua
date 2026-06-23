07eca0ee165426b40ad21f41c6b2856eb0035bdd
Eric <ericll360@gmail.com>
2026-06-24 08:07:51 +1000

    fix diffview stuff

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -117,60 +117,9 @@ vim.keymap.set('n', '<A-Left>', ':vertical resize -2<CR>')
 vim.keymap.set('n', '<A-Right>', ':vertical resize +2<CR>')
 
 vim.keymap.set("n", "<leader>df", "<cmd>DiffviewFileHistory %<cr>", { desc = "[D]iff [F]ile History (Current File)" })
-vim.keymap.set("n", "<leader>dc", function()
-  local ok, lib = pcall(require, "diffview.lib")
-  if not ok or not lib.get_current_view() then
-    return
-  end
-
-  local target_tab = lib.get_prev_non_view_tabpage()
-  if target_tab then
-    vim.api.nvim_set_current_tabpage(target_tab)
-  else
-    vim.notify("No non-Diffview tab to switch to", vim.log.levels.WARN)
-  end
-end, { desc = "[D]iff Hide ([C]ollapse)" })
+vim.keymap.set("n", "<leader>dc", "<cmd>tabprevious<cr>", { desc = "[D]iff [C]ollapse (prev tab)" })
 vim.keymap.set("n", "<leader>dx", "<cmd>DiffviewClose<cr>", { desc = "[D]iff Close" })
-vim.keymap.set("n", "<leader>dm", function()
-  local diff_args = { "main...HEAD", "--imply-local" }
-  local ok, lib = pcall(require, "diffview.lib")
-
-  if ok then
-    for _, view in ipairs(lib.views or {}) do
-      if view.rev_arg == "main...HEAD" and view.tabpage and vim.api.nvim_tabpage_is_valid(view.tabpage) then
-        local selected_file = view.panel and view.panel.cur_file and view.panel.cur_file.path
-        local panel_focused = view.panel and view.panel.is_focused and view.panel:is_focused()
-
-        view:close()
-        lib.dispose_view(view)
-
-        if selected_file then
-          vim.list_extend(diff_args, { "--selected-file", selected_file })
-        end
-
-        require("diffview").open(diff_args)
-
-        if not panel_focused then
-          vim.defer_fn(function()
-            local wins = vim.api.nvim_tabpage_list_wins(0)
-            for _, win in ipairs(wins) do
-              local bufnr = vim.api.nvim_win_get_buf(win)
-              local ft = vim.bo[bufnr].filetype
-              if ft ~= "DiffviewFiles" and ft ~= "DiffviewFileHistory" then
-                vim.api.nvim_set_current_win(win)
-                return
-              end
-            end
-          end, 100)
-        end
-
-        return
-      end
-    end
-  end
-
-  vim.cmd("DiffviewOpen main...HEAD --imply-local")
-end, { desc = "Diff against merge-base of main" })
+vim.keymap.set("n", "<leader>dm", "<cmd>DiffviewOpen main...HEAD --imply-local<cr>", { desc = "[D]iff against merge-base of [M]ain" })
 vim.keymap.set("n", "<leader>gy", function() Snacks.gitbrowse() end, { desc = "Git [Y]ank/Browse Link" })
 vim.keymap.set("v", "<leader>gy", function() Snacks.gitbrowse() end, { desc = "Git Browse (Selection)" })
 
