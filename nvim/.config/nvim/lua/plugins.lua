7b472205a785c530af0d2f349d4c1eaace66a9d5
Eric <ericll360@gmail.com>
2026-07-18 17:15:39 +1000

    message

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -11,13 +11,6 @@ require("lazy").setup({
     "88potatoes/agent-comments.nvim",
     dir = vim.fn.expand("~/Code/agent-comments.nvim"),
     config = function()
-      if vim.fn.executable("agent-comments") == 0 then
-        vim.notify(
-          "agent-comments.nvim: CLI not found. Install with: npm install -g agent-comments",
-          vim.log.levels.WARN
-        )
-        return
-      end
       require("agent-comments").setup()
     end,
   },
