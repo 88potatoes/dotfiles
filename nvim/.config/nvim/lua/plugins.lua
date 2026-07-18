f9060e8fd2775eb7b5679f21716669764e1cf924
Eric <ericll360@gmail.com>
2026-07-18 13:23:42 +1000

    refactor stuff

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -104,13 +104,6 @@ require("lazy").setup({
   {
     "sindrets/diffview.nvim",
     opts = {
-      enhanced_diff_hl = false,
-      view = {
-        merge_tool = {
-          layout = "diff3_horizontal",
-          disable_diagnostics = true,
-        },
-      },
       file_panel = {
         win_config = {
           position = "left",
@@ -143,43 +136,6 @@ require("lazy").setup({
         accept_suggestion = "<D-z>",
       }
     }
-  },
-  {
-    "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = "Trouble",
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    },
   },
   {
     "mfussenegger/nvim-lint",
